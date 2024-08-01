b1423bbcebb6759c29d0a85478ed836325ffdc05
thesivis <raphael@ic.ufmt.br>
2024-07-31 21:32:52 -0400

    Demonstra a utilização de referências

--- src/main/java/br/ufmt/alg3/App.java
+++ src/main/java/br/ufmt/alg3/App.java
@@ -1,5 +1,7 @@
 package br.ufmt.alg3;
 
+import java.util.Scanner;
+
 import br.ufmt.alg3.curso.Curso;
 import br.ufmt.alg3.professor.Professor;
 
@@ -7,9 +9,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        Curso c = new Curso("SI");
-        System.out.println(c.getNome());
-
-        System.out.println(c.getPeriodo());
+        Curso[] respostas;
+        respostas = new Curso[3];
+        Scanner teclado;
+        teclado = new Scanner(System.in);
+        for(int i = 0; i < respostas.length; i++) {
+            Curso c = new Curso();
+            System.out.print("Digite o nome do curso:");
+            String nome = teclado.nextLine();
+            c.setNome(nome);
+            System.out.println("O nome digitado foi:" + c.getNome());
+            respostas[i] = c;
+        }
+        for (Curso curso : respostas) {
+            System.out.println("Curso avaliado: " + curso.getNome());
+        }
     }
 }
